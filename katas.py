b32574d46031c0879f12950ac1e5e30aa3daf844
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-05-15 18:00:13 +0200

    Equal Sides Of An Array

--- katas.py
+++ katas.py
@@ -3,7 +3,15 @@
 
 def function_test():
     """Test functions here"""
-    print(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"))
+    print(find_even_index([1,2,3,4,3,2,1]))
+
+
+def find_even_index(arr):
+    """Kata: Equal Sides Of An Array"""
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i + 1:]):
+            return i
+    return - 1
 
 
 def order_weight(strng):
