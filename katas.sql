3c089fa675148b430424c20d1ff14fe13ad38aa2
Jan Knorr <jan.knorr@ramboll.com>
2021-01-28 17:59:22 +0100

    Kata: SQL Basics: Simple IN

--- katas.sql
+++ katas.sql
@@ -7,3 +7,19 @@ JOIN
   sales AS s ON p.id = s.people_id
 GROUP BY
   p.id, p.name;
+
+
+-- Kata: SQL Basics: Simple IN
+SELECT
+  id, name
+FROM
+  departments AS d
+WHERE
+  d.id IN (
+    SELECT
+      department_id
+    FROM
+      sales AS s
+    WHERE
+      s.price > 98
+  );
