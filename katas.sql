ca195632dfa34fff060c4b7ccadb7f6f38c1a9f0
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2021-01-29 18:05:23 +0100

    Kata: SQL Basics: Up and Down

--- katas.sql
+++ katas.sql
@@ -23,3 +23,18 @@ WHERE
     WHERE
       s.price > 98
   );
+
+
+  -- Kata: SQL Basics: Up and Down
+SELECT 
+  CASE
+    WHEN SUM(number1) % 2 = 0 THEN MAX(number1)
+    ELSE MIN(number1)
+  END AS number1,
+  CASE
+    WHEN SUM(number2) % 2 = 0 THEN MAX(number2)
+    ELSE MIN(number2)
+  END AS number2
+FROM
+  numbers;
+      
