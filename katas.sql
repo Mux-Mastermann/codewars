73abc701126bb3ef40c7d3806618979887a34bb6
Jan Knorr <jan.knorr@ramboll.com>
2021-02-01 17:09:24 +0100

    SQL Basics: Simple WITH

--- katas.sql
+++ katas.sql
@@ -59,3 +59,11 @@ FROM
 WHERE EXISTS
   (SELECT department_id FROM sales s WHERE d.id=s.department_id AND price > 98)
 ;
+
+
+-- SQL Basics: Simple WITH
+WITH special_sales AS (
+  SELECT * FROM sales WHERE price > 90
+)
+
+SELECT * FROM departments WHERE id IN (SELECT department_id FROM special_sales);
