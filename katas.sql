3e1938cf172e3c9483ef440de4e44e4b420983f8
Jan Knorr <jan.knorr@ramboll.com>
2021-02-12 17:59:28 +0100

    SQL Basics - Monsters using CASE

--- katas.sql
+++ katas.sql
@@ -209,3 +209,18 @@ SELECT
 FROM products
 GROUP BY producer
 ORDER BY count_products_types DESC, producer
+
+
+-- SQL Basics - Monsters using CASE
+SELECT t.id
+  ,t.heads
+  ,b.legs
+  ,t.arms
+  ,b.tails
+  ,CASE
+    WHEN t.heads > t.arms OR b.tails > b.legs THEN 'BEAST'
+    ELSE 'WEIRDO'
+  END AS species
+FROM top_half t
+JOIN bottom_half b ON t.id = b.id
+ORDER BY species;
